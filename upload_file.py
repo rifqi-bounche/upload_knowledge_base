8de708f4bf7b6bebadda4922699da6e0012a9e04
unknown <rifqi@bounche.com>
2025-09-09 11:07:05 +0700

    upload file

--- upload_file.py
+++ upload_file.py
@@ -1,11 +1,8 @@
 import streamlit as st
 from openai import OpenAI
-import os
-from dotenv import load_dotenv
 
-# 🔑 Load API Key dari .env
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+# 🔑 Ambil API key dari secrets
+api_key = st.secrets["OPENAI_API_KEY"]
 
 # Inisialisasi client
 client = OpenAI(api_key=api_key)
@@ -18,10 +15,10 @@ uploaded_file = st.file_uploader("Pilih file PDF untuk ditambahkan menjadi knowl
 if uploaded_file is not None:
     st.write("Nama file:", uploaded_file.name)
 
-    # Simpan sementara ke memory & kirim ke OpenAI
+    # Upload ke OpenAI vector store
     file_batch = client.vector_stores.file_batches.upload_and_poll(
         vector_store_id="vs_68ae8dab9e20819198991855e92be9da",
-        files=[uploaded_file]  # langsung pakai file-like object
+        files=[uploaded_file]
     )
 
     st.success("✅ File berhasil dimasukkan ke vector store!")
